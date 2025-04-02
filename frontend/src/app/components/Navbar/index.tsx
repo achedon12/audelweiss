1b75d50ced1a02a296692c6e4a8f99943971adc8
IPandragonI <mathysf84pro@gmail.com>
2025-04-02 18:23:16 +0200

    feat: Navbar responsive

--- frontend/src/app/components/Navbar/index.tsx
+++ frontend/src/app/components/Navbar/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {Dialog} from "@headlessui/react";
-import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
+import {List} from "phosphor-react";
 import {useState} from "react";
 import Logo from "@/app/components/Common/Logo";
 import {getStrapiMedia} from "@/app/utils/api-helpers";
@@ -33,14 +32,14 @@ interface MobileIconLink extends IconLink {
     closeMenu: () => void;
 }
 
-function NavLink({ url, text }: NavLink) {
+function NavLink({url, text}: NavLink) {
     const path = usePathname();
 
     return (
         <li className="flex">
             <Link
                 href={url}
-                className={`flex items-center mx-2 -mb-1 font-bold text-sm ${path === url && "text-awsalmon border-awsalmon"}}`}
+                className={`flex items-center mx-2 -mb-1 font-bold text-sm ${path === url && "text-awsalmon border-awsalmon"}`}
             >
                 {text}
             </Link>
@@ -48,7 +47,7 @@ function NavLink({ url, text }: NavLink) {
     );
 }
 
-function IconLink({ url, icon }: IconLink) {
+function IconLink({url, icon}: IconLink) {
     const iconUrl = getStrapiMedia(icon.url);
     return (
         <li className="flex">
@@ -56,29 +55,27 @@ function IconLink({ url, icon }: IconLink) {
                 href={url}
                 className="flex items-center mx-2 -mb-1 font-bold text-xs"
             >
-                <img src={iconUrl} alt={icon.alternativeText} className="h-6 w-6" />
+                <img src={iconUrl} alt={icon.alternativeText} className="h-6 w-6"/>
             </Link>
         </li>
     );
 }
 
-function MobileNavLink({ url, text, closeMenu }: MobileNavLink) {
+function MobileNavLink({url, text, closeMenu}: MobileNavLink) {
     const path = usePathname();
     const handleClick = () => {
         closeMenu();
     };
     return (
-        <a className="flex">
+        <div className="flex">
             <Link
                 href={url}
                 onClick={handleClick}
-                className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-900 ${
-                    path === url && "dark:text-violet-400 dark:border-violet-400"
-                }}`}
+                className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-awblack ${path === url && "text-awpink"}`}
             >
                 {text}
             </Link>
-        </a>
+        </div>
     );
 }
 
@@ -94,11 +91,21 @@ export default function Navbar({links, logoUrl, logoText, iconLinks}: {
     };
 
     return (
-        <div className="py-4 px-10 dark:bg-black dark:text-gray-100">
+        <div className="py-4 px-10 dark:bg-awgray dark:text-awgray">
             <div className="container flex justify-between h-16 mx-auto px-0 sm:px-6">
                 <Logo src={logoUrl}>
                     {logoText && <h2 className="text-2xl font-bold">{logoText}</h2>}
                 </Logo>
+                <div className="flex items-center lg:hidden">
+                    <div className="flex">
+                        {iconLinks.map((item: IconLink) => (
+                            <IconLink key={item.id} {...item} />
+                        ))}
+                    </div>
+                    <button className={"px-2"} onClick={() => setMobileMenuOpen(true)}>
+                        <List size={26} className="text-awblack" aria-hidden="true"/>
+                    </button>
+                </div>
                 <div className="items-center flex-shrink-0 hidden lg:flex">
                     <ul className="items-stretch hidden space-x-1 lg:flex">
                         {links.map((item: NavLink) => (
@@ -111,37 +118,22 @@ export default function Navbar({links, logoUrl, logoText, iconLinks}: {
                         </div>
                     </ul>
                 </div>
-                <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                    <div className="fixed inset-0 z-40 bg-gray-600 bg-opacity-75" />
-                    <div className="fixed inset-y-0 rtl:left-0 ltr:right-0 z-50 w-full overflow-y-auto bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-inset sm:ring-white/10">
-                        <div className="flex items-center justify-between">
-                            <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Strapi</span>
-                                {logoUrl && <img className="h-8 w-auto" src={logoUrl} alt="" />}
-                            </a>
-                            <button
-                                type="button"
-                                className="-m-2.5 rounded-md p-2.5 text-white"
-                                onClick={() => setMobileMenuOpen(false)}
-                            >
-                                <span className="sr-only">Close menu</span>
-                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div className="mt-6 flow-root">
-                            <div className="-my-6 divide-y divide-gray-700">
-                                <div className="space-y-2 py-6">
-                                    {links.map((item) => (
-                                        <MobileNavLink key={item.id} closeMenu={closeMenu} {...item} />
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
+
+                <div className={`fixed inset-0 z-10 flex items-center justify-center bg-opacity-50 transition-opacity duration-300 ${mobileMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
+                    <div className={`fixed top-0 right-0 mt-16 mr-4 w-11/12 bg-white border-t border-t-awpink p-4 transform transition-transform ${mobileMenuOpen ? "translate-y-10" : "-translate-y-10"} shadow-lg`}>
+                        <button className="absolute top-4 right-4 text-gray-500" onClick={closeMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                        </button>
+                        <ul className="space-y-4">
+                            {links.map((item: NavLink) => (
+                                <MobileNavLink key={item.id} {...item} closeMenu={closeMenu} />
+                            ))}
+                        </ul>
                     </div>
-                </Dialog>
-                <button className="p-4 lg:hidden" onClick={() => setMobileMenuOpen(true)}>
-                    <Bars3Icon className="h-7 w-7 text-gray-100" aria-hidden="true" />
-                </button>
+                </div>
+
             </div>
         </div>
     );
