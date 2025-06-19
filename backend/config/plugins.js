956e5250a9cecdc42c3ed8840f16a1a5fb583490
Achedon12 <leoderoinleo@gmail.com>
2025-06-19 11:32:02 +0200

    feat: strapi mailer provider

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    email: {
+        config: {
+            provider: "nodemailer",
+            providerOptions: {
+                host: env("EMAIL_SMTP_HOST", "localhost"),
+                port: env.int("EMAIL_SMTP_PORT", 1024),
+                secure: env.bool("EMAIL_SMTP_SECURE", false), // true si TLS/SSL (port 465)
+                auth: {
+                    user: env("EMAIL_SMTP_USER", "mailhog"), // Utiliser "mailhog" pour MailHog
+                    pass: env("EMAIL_SMTP_PASS", "mailhog"), // Utiliser "mailhog" pour MailHog
+                },
+            },
+            settings: {
+                defaultFrom: env("EMAIL_ADDRESS_FROM", "no-reply@audeilwess.com"),
+                defaultReplyTo: env("EMAIL_ADDRESS_REPLY", "support@audeilwess.com"),
+            },
+        },
+    },
+});
